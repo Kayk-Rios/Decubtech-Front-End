0171f2d7b183530fe6e496751fdc4fa81d884b4e
Kayk-Rios <manjac688@gmail.com>
2024-10-20 02:37:32 -0300

    feaf:listaSetores

---
+++ src/components/data/listaSetor.ts
@@ -0,0 +1,18 @@
+export interface HospitalSector {
+    id: number;
+    name: string;
+  }
+  
+  export const hospitalSectors: HospitalSector[] = [
+    { id: 1, name: "Emergência" },
+    { id: 2, name: "UTI" },
+    { id: 3, name: "Pediatria" },
+    { id: 4, name: "Oncologia" },
+    { id: 5, name: "Cardiologia" },
+    { id: 6, name: "Radiologia" },
+    { id: 7, name: "Ortopedia" },
+    { id: 8, name: "Maternidade" },
+    { id: 9, name: "Centro Cirúrgico" },
+    { id: 10, name: "Hematologia" }
+  ];
+  
