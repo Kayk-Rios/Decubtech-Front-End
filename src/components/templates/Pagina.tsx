e83fa5376c49d01f0e8ac5bc8dbd492a542f7b75
Kayk-Rios <manjac688@gmail.com>
2024-10-23 12:55:39 -0300

    style:PaginaEnvolverComponentes

--- src/components/templates/Pagina.tsx
+++ src/components/templates/Pagina.tsx
@@ -1,12 +1,11 @@
-import Link from "next/link"
-import Menu from "./Menu"
+ 
 
 
 
 export default function Pagina(props:any){
     return(
         <>
-        <div>
+        <div className=" sm:flex flex-col md:block">
             <main>{props.children}</main>
         </div>
         </>
