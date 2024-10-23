b227b181d5e46ad71988390e678c1a57e6b6b1e2
Kayk-Rios <manjac688@gmail.com>
2024-10-23 12:50:50 -0300

    feat:telaPaciente

---
+++ src/app/layout/paciente/page.tsx
@@ -0,0 +1,23 @@
+import Menu from "@/components/templates/Menu";
+import Pagina from "@/components/templates/Pagina";
+import Entrar from "@/components/Ui/Entrar";
+import Link from "next/link";
+
+export default function PacientePagina() {
+  return (
+    <>
+      <div>
+        <Pagina>
+            <Entrar destino="Pacientes" className=" flex text-slate-200 justify-center bg-pink-400 mt-[4%] ml-[2%]"/>
+          <div className="flex flex-col w-16 ml-[83%] gap-3">
+            
+            <Menu/>
+            <Link href="/" className=" mb-10">
+              <h1 className="sair rounded-md ">Sair</h1>
+            </Link>
+          </div>
+        </Pagina>
+      </div>
+    </>
+  );
+}
